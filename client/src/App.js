458346956d3bc881bc145285f5d542fbfd691aa2
Paul N Barnes <paulieb99@gmail.com>
2023-01-15 19:48:19 -0800

    added redux, react-router-dom, react-bootstrap, and other dependencies for app functionality. tested app and have display of the hompage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
