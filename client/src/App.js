5d947561d3061e84bc7fda0f9e50401cb1a77d02
Paul N Barnes <paulieb99@gmail.com>
2023-01-15 20:16:55 -0800

    adding navbar links and LinkContainer from react-router-bootstrap for react router functionality. added justify class name to small links for spacing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
       </Routes>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, Navbar, NavdropDown, Container } from 'react-bootstrap';
 
 const Header = () => {
-  return;
+  return (
+    <Container>
+      <LinkContainer to='/'>
+        <Navbar.Brand>Paul Barnes</Navbar.Brand>
+      </LinkContainer>
+      <Nav className='justify-content-end'>
+        <LinkContainer to='/projects'>
+          <Nav.Link>Projects</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/developers'>
+          <Nav.Link>Developers</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/contact'>
+          <Nav.Link>Contact</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </Container>
+  );
 };
 
 export default Header;
