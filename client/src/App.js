3f62ed088fbee30da44f13e6eb80af02e0c05c68
Paul N Barnes <paulieb99@gmail.com>
2023-01-15 20:35:29 -0800

    created the header with basic react-bootstrap styling, added bootstrap css file to display styles. ran test and it displays to page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-      </Routes>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomePage />}></Route>
+          </Routes>
+        </Container>
+      </main>
     </Router>
   );
 }
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Nav, Navbar, NavdropDown, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <Container>
-      <LinkContainer to='/'>
-        <Navbar.Brand>Paul Barnes</Navbar.Brand>
-      </LinkContainer>
-      <Nav className='justify-content-end'>
-        <LinkContainer to='/projects'>
-          <Nav.Link>Projects</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to='/developers'>
-          <Nav.Link>Developers</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to='/contact'>
-          <Nav.Link>Contact</Nav.Link>
-        </LinkContainer>
-      </Nav>
-    </Container>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Paul Barnes</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/projects'>
+                <Nav.Link>Projects</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/developers'>
+                <Nav.Link>Developers</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/contact'>
+                <Nav.Link>Contact</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
