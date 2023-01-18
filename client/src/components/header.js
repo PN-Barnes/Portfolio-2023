a97e955f4a5c15f2ea6fad3b9425d2894be31f00
Paul N Barnes <paulieb99@gmail.com>
2023-01-17 20:06:54 -0800

    added styling for the header.js

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import './header.css';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 
 const Header = () => {
+  const headerLinkStyles = {
+    color: 'black',
+    fontWeight: '700',
+    fontSize: 'larger',
+    transition: 'font-size 3s',
+  };
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar as='headerNavBar' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Paul Barnes</Navbar.Brand>
+            <Navbar.Brand className='navBrand'>Paul Barnes</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <LinkContainer to='/projects'>
+              <LinkContainer
+                className='linkContainer'
+                style={headerLinkStyles}
+                to='/projects'
+              >
                 <Nav.Link>Projects</Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/developers'>
+              <LinkContainer
+                className='linkContainer'
+                style={headerLinkStyles}
+                to='/developers'
+              >
                 <Nav.Link>Developers</Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/contact'>
+              <LinkContainer
+                className='linkContainer'
+                style={headerLinkStyles}
+                to='/contact'
+              >
                 <Nav.Link>Contact</Nav.Link>
               </LinkContainer>
             </Nav>
